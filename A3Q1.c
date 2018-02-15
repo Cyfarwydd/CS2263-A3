67420159f3a01d73c5cf58c2b3a91c3a97435e11
Nicolas MacDonald <nicdmacdonald@gmail.com>
2018-02-15 11:36:47 -0400

    added chomp function to remove \n from stdin, moved all variables to the top to be declared right away for clarity

--- A3Q1.c
+++ A3Q1.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_WORD_SIZE 20
-
+char* chomp(char* str);
 int main(int argc, char *argv[]){
+	const char *restrict filename;
+	FILE *reviews;
+	char word[MAX_WORD_SIZE] = "";
+	char match[MAX_WORD_SIZE];
+	long int score = 0;
+	int scorecnt = 0;
+
 	if(argc!=2){
 		printf("not enough arguments\n");
 		exit(0);
 	}
-	const char * restrict filename = argv[1];
-		
-	printf("%s", filename);
-	FILE * reviews; 
+	filename = argv[1];
 	if(!(reviews = fopen(filename, "r"))){
 		printf("no file found\n");
 		exit(0);
 	}
 	printf("Enter a word: ");
-	char word[MAX_WORD_SIZE] = "";
 	while(!(fgets(word, MAX_WORD_SIZE, stdin))){
 		printf("\nTry again: ");
 	}
-	int i;
-	for(i = 0; word[i] != '\0'; i++){
-		if(word[i] == '\n'){
-			word[i] = '\0';
-		}
-	}
-	char match[MAX_WORD_SIZE];
+	chomp(word);
 	while(fgets(match, MAX_WORD_SIZE, reviews)){
 		char* pword = word;
 		char* pmatch = match;
 		while(*pword == *pmatch && (*pmatch != ' ' || *pword != ' ')){
-			printf("win\n");
 			pword++;
 			pmatch++;
+			if(*pmatch==' '||*pmatch == '\n'){
+				
+			}
 		}
 	}
+}
+char * chomp(char * str){
+	char* p;
+	for(p = str; *p != '\n'; p++);
+	*p = '\0';
+	return str;
 }
